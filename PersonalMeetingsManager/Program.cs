72a161cc28727ca8795a1a97a510f8752e9d0a83
680zx <stas.lekontsv@gmail.com>
2020-12-14 13:00:47 +0400

    Initial commit.

---
+++ PersonalMeetingsManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalMeetingsManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
