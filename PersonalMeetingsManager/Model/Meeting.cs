5af34e4e0bf282e5cd4db3771fef5e9c81903075
680zx <stas.lekontsv@gmail.com>
2020-12-15 21:03:21 +0400

    Add Meeting class constructor

--- PersonalMeetingsManager/Model/Meeting.cs
+++ PersonalMeetingsManager/Model/Meeting.cs
@@ -6,8 +6,19 @@ namespace PersonalMeetingsManager
 {
     public class Meeting
     {
-        public DateTime startDateTime { get; set; }
-        public DateTime endDateTime { get; set; }
-        public DateTime reminderTime { get; set; }
+        private DateTime _startDateTime;
+        private DateTime _endDateTime;
+        private DateTime _reminderDateTime;
+
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public DateTime ReminderTime { get; set; }
+
+        public Meeting(DateTime startDateTime, DateTime endDateTime, DateTime reminderTime)
+        {
+            _startDateTime = startDateTime;
+            _endDateTime = endDateTime;
+            _reminderDateTime = reminderTime;
+        }
     }
 }
